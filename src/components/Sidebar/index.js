401bac0ce864720e39f68c4a796ffab74083bd3b
Ruchit Sheth <ruchitsheth127@gmail.com>
2025-04-07 22:53:55 +0530

    completed left sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,14 +1,73 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './index.scss';
-import LogoR from '../../assets/images/logo-r.jpg';
+import LogoS from '../../assets/images/logo-s.png';
 import LogoSubtitle from '../../assets/images/logo_sub.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faLinkedin,
+    faGithub,
+    faKaggle,
+  } from '@fortawesome/free-brands-svg-icons';
+  import {
+    faHome,
+    faUser,
+    faEnvelope,
+    faSuitcase,
+    faBars,
+    faClose,
+  } from '@fortawesome/free-solid-svg-icons';
 
 const Sidebar = () => (
     <div className='nav-bar'>
         <Link className='logo' to='/'>
-            <img src={LogoR} alt='logo' />
-            <img src={LogoSubtitle} alt='ruchit' />
+            <img src={LogoS} alt='logo' />
+            <img className="sub-logo" src={LogoSubtitle} alt='ruchit' />
         </Link>
+        <nav>
+            <NavLink exact="true" activeClassName="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeClassName="active" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+            </NavLink>
+            <NavLink 
+                exact="true" 
+                activeClassName="active" 
+                className="contact-link" 
+                to="/contact"
+            >
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+            </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a 
+                    target="_blank" 
+                    rel='noreferrer' 
+                    href='https://www.linkedin.com/in/ruchit-sheth-/'
+                >
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a 
+                    target="_blank" 
+                    rel='noreferrer' 
+                    href='https://github.com/ruchitsheth13'
+                >
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a 
+                    target="_blank" 
+                    rel='noreferrer' 
+                    href='https://www.kaggle.com/ruchitsheth1'
+                >
+                    <FontAwesomeIcon icon={faKaggle} color="#4d4d4e" />
+                </a>
+            </li>
+        </ul>
     </div>
 )
 
